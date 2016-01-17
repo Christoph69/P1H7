e636750cd306f7ccca12ba7e325c0babb9e4623b
Christoph Wagner <christoph@Christophs-iMac.local>
2016-01-17 21:41:38 +0100

    Dateien erstellt

---
+++ inc/train.h
@@ -0,0 +1,5 @@
+#ifndef TRAIN_H
+#define TRAIN_H
+
+
+#endif /* ifndef TRAIN_H */
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+// main.cpp zu P1H7
+#include "../inc/list.h"
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
